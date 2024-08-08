2211041188b74ccb7db701e69ff78b0b0cfdb1f4
jayleensusie <Bloxnetwork3@gmail.com>
2024-08-08 12:50:13 +0300

    Initial DB Structure

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,104 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable, NOW } from 'astro:db';
+
+const Chain = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text({ multiline: false }),
+    created_at: column.date({ default: NOW }),
+    updated_at: column.date({ default: NOW }),
+  },
+});
+
+const Token = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    chain_id: column.number(),
+    symbol: column.text({ multiline: false }),
+    name: column.text({ multiline: false }),
+    created_at: column.date({ default: NOW }),
+    updated_at: column.date({ default: NOW }),
+  },
+  foreignKeys: [
+    {
+      columns: ['chain_id'],
+      references: () => [Chain.columns.id],
+    },
+  ],
+  indexes: [{ on: ['chain_id', 'symbol'], unique: true }],
+});
+
+const Presale = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text({ multiline: false }),
+    starts_at: column.date(),
+    ends_at: column.date({ optional: true }),
+    price: column.number(),
+    minimum_buy: column.number({ optional: true }),
+    maximum_buy: column.number({ optional: true }),
+    total_supply: column.number(),
+    vesting_initial_unlock: column.number(),
+    vesting_starts_after: column.number(),
+    vesting_period: column.number(),
+    vesting_period_count: column.number(),
+    created_at: column.date({ default: NOW }),
+    updated_at: column.date({ default: NOW }),
+  },
+});
+
+const PresaleAddress = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    presale_id: column.number(),
+    chain_id: column.number(),
+    address: column.text({ multiline: false }),
+    created_at: column.date({ default: NOW }),
+    updated_at: column.date({ default: NOW }),
+  },
+  foreignKeys: [
+    {
+      columns: ['presale_id'],
+      references: () => [Presale.columns.id],
+    },
+    {
+      columns: ['chain_id'],
+      references: () => [Chain.columns.id],
+    },
+  ],
+});
+
+const Purchase = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    hash: column.text(),
+    chain_id: column.number(),
+    token_symbol: column.text({ multiline: false }),
+    presale_id: column.number({ references: () => Presale.columns.id }),
+    address: column.text({ multiline: false }),
+    amount: column.number(),
+    purchased_at: column.date(),
+  },
+  foreignKeys: [
+    {
+      columns: ['chain_id', 'token_symbol'],
+      references: () => [Token.columns.chain_id, Token.columns.symbol],
+    },
+  ],
+  indexes: [{ on: ['chain_id', 'hash'], unique: true }],
+});
+
+const Claim = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    address: column.text(),
+    unlocked_at: column.date(),
+    amount: column.number(),
+    hash: column.text({ multiline: false, optional: true }),
+    claimed_at: column.date({ optional: true }),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Chain, Token, Presale, PresaleAddress, Purchase, Claim },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,88 @@
-import { db } from 'astro:db';
+import { Chain, Presale, PresaleAddress, Token, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  const chains = [
+    { id: 1, name: 'Ethereum' },
+    { id: 10, name: 'OP Mainnet' },
+    { id: 56, name: 'Binance Smart Chain' },
+    { id: 137, name: 'Polygon Mainnet' },
+    { id: 8453, name: 'Base' },
+    { id: 42161, name: 'Arbitrum One' },
+    { id: 59144, name: 'Linea' },
+  ];
+  await db.insert(Chain).values(chains);
+
+  await db.insert(Token).values([
+    // Mainnet Currencies
+    { id: 1, chain_id: 1, symbol: 'ETH', name: 'ETH (Ethereum)' },
+    { id: 2, chain_id: 10, symbol: 'ETH', name: 'ETH (OP Mainnet)' },
+    { id: 3, chain_id: 56, symbol: 'BNB', name: 'BNB (Binance Smart Chain)' },
+    { id: 4, chain_id: 137, symbol: 'MATIC', name: 'MATIC (Polygon Mainnet)' },
+    { id: 5, chain_id: 8453, symbol: 'ETH', name: 'ETH (Base)' },
+    { id: 6, chain_id: 42161, symbol: 'ETH', name: 'ETH (Arbitrum One)' },
+    { id: 7, chain_id: 59144, symbol: 'ETH', name: 'ETH (Linea)' },
+    // Extra Currencies
+    { id: 8, chain_id: 1, symbol: 'USDC', name: 'USDC (Ethereum)' },
+    { id: 9, chain_id: 1, symbol: 'USDT', name: 'USDT (Ethereum)' },
+  ]);
+
+  const presales = [
+    {
+      id: 1,
+      name: 'Presale Round 1',
+      starts_at: new Date('2024-08-10'),
+      ends_at: null,
+      price: 0.016,
+      minimum_buy: 4_000,
+      maximum_buy: 1_000_000,
+      total_supply: 35_000_000,
+      vesting_initial_unlock: 10 / 100,
+      vesting_starts_after: 3,
+      vesting_period: 2,
+      vesting_period_count: 3,
+    },
+    {
+      id: 2,
+      name: 'Presale Round 2',
+      starts_at: new Date('2024-08-20'),
+      ends_at: null,
+      price: 0.024,
+      minimum_buy: 2_000,
+      maximum_buy: 600_000,
+      total_supply: 35_000_000,
+      vesting_initial_unlock: 17.5 / 100,
+      vesting_starts_after: 2,
+      vesting_period: 1,
+      vesting_period_count: 4,
+    },
+    {
+      id: 3,
+      name: 'Presale Round 3',
+      starts_at: new Date('2024-08-30'),
+      ends_at: null,
+      price: 0.032,
+      minimum_buy: 1_000,
+      maximum_buy: 400_000,
+      total_supply: 40_000_000,
+      vesting_initial_unlock: 25 / 100,
+      vesting_starts_after: 0,
+      vesting_period: 1,
+      vesting_period_count: 3,
+    },
+  ];
+  await db.insert(Presale).values(presales);
+
+  const addresses = [];
+  for (const presale of presales) {
+    for (const chain of chains) {
+      addresses.push({
+        id: addresses.length + 1,
+        chain_id: chain.id,
+        presale_id: presale.id,
+        address: '0x0000000000000000000000000000000000000000',
+      });
+    }
+  }
+  await db.insert(PresaleAddress).values(addresses);
 }
